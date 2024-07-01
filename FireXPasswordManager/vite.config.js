8d20783239e8c71282523d2915a96fa4aa15483e
sathruwancooray <sathruwancooray@gmail.com>
2024-07-01 13:51:22 +0530

    customize for deploy

--- FireXPasswordManager/vite.config.js
+++ FireXPasswordManager/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // This ensures assets are loaded correctly
   css: {
     preprocessorOptions: {
       less: {
